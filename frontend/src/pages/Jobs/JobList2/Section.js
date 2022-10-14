4f37f917ccea79123c6919a535d342a293ac5c3d
waflol <lenguyenhunganh2001@gmail.com>
2022-10-14 16:51:51 +0700

    update

--- frontend/src/pages/Jobs/JobList2/Section.js
+++ frontend/src/pages/Jobs/JobList2/Section.js
@@ -10,12 +10,12 @@ const Section = () => {
                             <Row className="justify-content-center">
                                 <Col md={6}>
                                     <div className="text-center text-white">
-                                        <h3 className="mb-4">Job Description</h3>
+                                        <h3 className="mb-4">Job List</h3>
                                         <div className="page-next">
                                             <nav className="d-inline-block" aria-label="breadcrumb text-center">
                                                 <ol className="breadcrumb justify-content-center">
                                                     <li className="breadcrumb-item"><Link to="/">Home</Link></li>
-                                                    <li className="breadcrumb-item active" aria-current="page"> Job Description </li>
+                                                    <li className="breadcrumb-item active" aria-current="page"> Job List </li>
                                                 </ol>
                                             </nav>
                                         </div>
